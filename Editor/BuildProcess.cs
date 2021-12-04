a453984ed6e857f2ccd10ad541c194fca94f9f06
Castle <castle.wsc@gmail,com>
2021-12-04 18:02:53 +0800

    update to 1.0.3

--- Editor/BuildProcess.cs
+++ Editor/BuildProcess.cs
@@ -5,85 +5,33 @@ using UnityEditor.Build.Reporting;
 using System;
 using System.IO;
 
-namespace Lab5Games.VersionBuilder.Editor
+namespace Lab5Games.Editor
 {
     public class BuildProcess : IPreprocessBuildWithReport
     {
-        public int callbackOrder { get { return 0; } }
+        public int callbackOrder => 0;
 
-        
         public void OnPreprocessBuild(BuildReport report)
         {
-            var asset = AssetDatabase.LoadAssetAtPath<AppVersion>("Assets/Resources/AppVersion.asset");
+            string strVersion = PlayerSettings.bundleVersion;
 
-            if (asset == null)
-                throw new NullReferenceException("AppVersion asset is null");
+            AppVersion appVersion = new AppVersion(strVersion);
 
+            if (appVersion.status == "ERROR")
+                appVersion = new AppVersion(0, 0, 0, "DEBUG");
 
+            ++appVersion.build;
+            PlayerSettings.bundleVersion = appVersion.VersionCodeFull;
 
             switch(EditorUserBuildSettings.activeBuildTarget)
             {
-                case BuildTarget.StandaloneWindows:
-                case BuildTarget.StandaloneWindows64:
-                    ++asset.Windows.patch;
-                    asset.Windows.date = DateTime.Now.ToString("yyyy-mm-dd");
-
-                    PlayerSettings.bundleVersion = asset.Windows.GetVersionCode();
-
-                    Debug.LogWarning($"Build new version: {asset.Windows.GetVersionCodeFull()}");
-                    break;
-
                 case BuildTarget.iOS:
-                    ++asset.iOS.patch;
-                    asset.iOS.date = DateTime.Now.ToString("yyyy-mm-dd");
-
-                    PlayerSettings.bundleVersion = asset.iOS.GetVersionCode();
-                    PlayerSettings.iOS.buildNumber = asset.iOS.patch.ToString();
-
-                    Debug.LogWarning($"Build new version: {asset.iOS.GetVersionCodeFull()}");
+                    PlayerSettings.iOS.buildNumber = appVersion.build.ToString();
                     break;
 
                 case BuildTarget.Android:
-                    ++asset.Android.patch;
-                    asset.Android.date = DateTime.Now.ToString("yyyy-mm-dd");
-
-                    PlayerSettings.bundleVersion = asset.Android.GetVersionCode();
-                    PlayerSettings.Android.bundleVersionCode = asset.iOS.patch;
-
-                    Debug.LogWarning($"Build new version: {asset.Android.GetVersionCodeFull()}");
+                    PlayerSettings.Android.bundleVersionCode = appVersion.build;
                     break;
-
-                case BuildTarget.WebGL:
-                    ++asset.WebGL.patch;
-                    asset.WebGL.date = DateTime.Now.ToString("yyyy-mm-dd");
-
-                    PlayerSettings.bundleVersion = asset.WebGL.GetVersionCode();
-
-                    Debug.LogWarning($"Build new version: {asset.WebGL.GetVersionCodeFull()}");
-                    break;
-            }
-
-
-            EditorUtility.SetDirty(asset);
-            AssetDatabase.SaveAssets();
-        }
-
-
-        [InitializeOnLoadMethod]
-        private static void CreateDefaultAsset()
-        {
-            if (AssetDatabase.LoadAssetAtPath<AppVersion>("Assets/Resources/AppVersion.asset") == null)
-            {
-                var asset = ScriptableObject.CreateInstance<AppVersion>();
-
-                if(!AssetDatabase.IsValidFolder("Assets/Resources"))
-                {
-                    AssetDatabase.CreateFolder("Assets", "Resources");
-                }
-
-                AssetDatabase.CreateAsset(asset, "Assets/Resources/AppVersion.asset");
-                AssetDatabase.SaveAssets();
-                AssetDatabase.Refresh();
             }
         }
     }
--- Runtime/AppVersion.cs
+++ Runtime/AppVersion.cs
@@ -1,36 +1,42 @@
-using System;
-using System.IO;
 using UnityEngine;
 
-namespace Lab5Games.VersionBuilder
+namespace Lab5Games
 {
-    [System.Serializable]
-    public class AppVersion : ScriptableObject
+    public class AppVersion 
     {
-        [System.Serializable]
-        public class VersionFormat
+        public int major;
+        public int minor;
+        public int build;
+        public string status;
+
+        public string VersionCodeFull => $"{major}.{minor}.{build} - {status}";
+
+        public AppVersion(int major, int minor, int patch, string status)
         {
-            public int major;
-            public int minor;
-            public int patch;
-
-            public string status;
-            public string date;
-
-            public string GetVersionCode()
-            {
-                return $"{major}.{minor}.{patch}";
-            }
-
-            public string GetVersionCodeFull()
-            {
-                return $"{major}.{minor}.{patch} [{status}]";
-            }
+            this.major = major;
+            this.minor = minor;
+            this.build = patch;
+            this.status = status;
         }
 
-        public VersionFormat Windows;
-        public VersionFormat iOS;
-        public VersionFormat Android;
-        public VersionFormat WebGL;
+        public AppVersion(string strVersion)
+        {
+            string[] splitArray = strVersion.Split('.','-');
+
+            if (splitArray.Length != 4)
+            {
+                this.major = 0;
+                this.minor = 0;
+                this.build = 0;
+                this.status = "ERROR";
+            }
+            else
+            {
+                this.major = int.Parse(splitArray[0].Trim());
+                this.minor = int.Parse(splitArray[1].Trim());
+                this.build = int.Parse(splitArray[2].Trim());
+                this.status = splitArray[3].Trim();
+            }
+        }
     }
 }
